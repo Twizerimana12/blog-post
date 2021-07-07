1367ebfaf1aa124c40f62c0d66dcd5fab62bc1b5
Twizerimana12 <cleniatwizerimana@gmail.com>
2021-07-06 23:06:49 -0600

    blog assignment in she can code

---
+++ copy.java
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>my first java?</h2>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "hello my friend of mine!"'>Click Me!</button>
+
+</body>
+
+
+</html>
